4f396b599299ee8471edeafd78e88605794da8da
marisanesmith <marisa@thisworldtraveled.com>
2021-04-10 13:28:50 -0400

    initial setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const morgan = require("morgan");
+
+const PORT = process.env.PORT || 3000
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes (check route)
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
