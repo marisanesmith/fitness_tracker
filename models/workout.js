37e3ee299aa0811ae981ee6464fed55fb1bb46f3
marisanesmith <marisa@thisworldtraveled.com>
2021-04-12 15:54:55 -0400

    updated workout models

--- models/workout.js
+++ models/workout.js
@@ -2,15 +2,30 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
+const workoutSchema = new Schema(
+    {
     day: {
         type: Date,
         default: Date.now()
     },
-    exercises: {
-        type: Array,
-        default: []
-    }
+    exercises: [
+        {
+         type: {
+             type: String,
+             trim: true,
+             required: "Please enter a type of exercise",
+         },
+         name: {
+             type: String,
+             trim: true,
+             required: "Please enter the name of the exercise",
+         },
+         duration: {
+             type: Number,
+             required: "Please enter the duration of the exercise in minutes",
+         },
+         },
+    ]
 },
 {
     toJSON: {
