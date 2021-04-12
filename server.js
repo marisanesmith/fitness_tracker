d2ec736c264853dfa8789935f01db914d9a8ee00
marisanesmith <marisa@thisworldtraveled.com>
2021-04-12 15:42:58 -0400

    updated server.js

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 
 // routes (check route)
 app.use(require("./routes/apiRoutes.js"));
+app.use(require(".routes/htmlRoutes"))
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
